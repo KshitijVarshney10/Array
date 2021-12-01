462eda411c7e59e535c1a7726c69f3e1340ba0aa
Kshitij Varshney <kshitij.varshney1007@gmail.com>
2021-12-01 22:40:15 +0530

    Initial commit

---
+++ src/Palindrome.java
@@ -0,0 +1,21 @@
+/* Write a Java program to find if the given number is palindrome or not. */
+
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n=sc.nextInt();
+        int r,sum=0,temp;
+        temp=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
+}
